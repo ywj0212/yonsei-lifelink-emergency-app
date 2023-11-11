d860b1b49d8810d0602f31311d230a1d2564a5de
유우진 <ywj02120212@gmail.com>
2023-11-12 02:54:12 +0900

    fix: Relative Positon of Click Error

--- src/App.js
+++ src/App.js
@@ -13,22 +13,26 @@ function App() {
   }
 
   return (<>
+    <header style={{  position: "sticky", top: 0, fontSize: 15, color: "white", backgroundColor: "#202", margin: 0, zIndex: 2 }}>
+      <pre>
+        <p>x: {localXY.x.toFixed(2)} | {xy.x.toFixed(2)} | ({((localXY.x / size.x)*100).toFixed(2)}%)</p>
+        <p>y: {localXY.y.toFixed(2)} | {xy.y.toFixed(2)} | ({((localXY.y / size.y)*100).toFixed(2)}%)</p>
+      </pre>
+    </header>
     <img
       src="map.png"
       alt="map"
       onClick={(e) => {
         setSize({x: e.target.width, y: e.target.height});
-        setXY({x:e.clientX,y:e.clientY});
-        setLocalXY({x: e.clientX - e.target.offsetLeft, y: e.clientY - e.target.offsetTop});
+        setXY({x:e.pageX, y:e.pageY});
+        setLocalXY({x: e.pageX - e.target.offsetLeft, y: e.pageY - e.target.offsetTop});
         }
       }
-      style={{position: "absolute", left: 10, top: 150, width: "80%", height: "auto"}}
+      style={{ width: "100%", height: "auto" }}
       />
 
-    <div style={{width: '15px', height: '15px', display: "inline", borderRadius: '7.5px', backgroundColor: "red", position: "absolute", left: xy.x - 7.5, top: xy.y - 7.5}}></div>
+    <div style={{ width: '15px', height: '15px', display: "inline", borderRadius: '7.5px', backgroundColor: "red", position: "absolute", left: xy.x - 7.5, top: xy.y - 7.5}}></div>
 
-    <p>x: {localXY.x}, {xy.x}, ({(localXY.x / size.x)*100}%)</p>
-    <p>y: {localXY.y}, {xy.y}, ({(localXY.y / size.y)*100}%)</p>
     <p>lat: {lerp(lt.x, rb.x, (localXY.x / size.x))}</p>
     <p>lon: {lerp(lt.y, rb.y, (localXY.y / size.y))}</p>
   </>)
