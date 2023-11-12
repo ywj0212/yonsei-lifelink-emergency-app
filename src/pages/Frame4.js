71c8da12a47f82159efcba5ea977a3a30cc162e8
유우진 <ywj02120212@gmail.com>
2023-11-13 01:20:52 +0900

    fix: Frame4

--- src/pages/Frame4.js
+++ src/pages/Frame4.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import styles from "./Frame4.module.css";
 import { Link } from "react-router-dom";
 
 const Frame4 = () => {
+  const [sex, setSex] = useState(null);
+
   return (
     <div className={styles.div}>
       <div className={styles.inner}>
@@ -24,11 +27,13 @@ const Frame4 = () => {
           </div>
           <div className={styles.groupChild1} />
           <div className={styles.div3}>성별</div>
-          <div className={styles.groupChild2} />
-          <div className={styles.groupChild3} />
-          <button className={styles.div4}>남</button>
-          <button className={styles.div5}>여</button>
-          <Link to="/symptom">
+          <div className={styles.groupChild2} style={(sex === "M" ? {backgroundColor: "#ff7979"} : {backgroundColor: "#e2e2e2"})} onClick={() => setSex("M")} />
+          <div className={styles.div4} style={{backgroundColor: "transparent"}}>남</div>
+
+          <div className={styles.groupChild3} style={(sex === "F" ? {backgroundColor: "#ff7979"} : {backgroundColor: "#e2e2e2"})} onClick={() => setSex("F")} />
+          <div className={styles.div5} style={{backgroundColor: "transparent"}}>여</div>
+
+          <Link to="/symptom" onClick={(e) => {if (sex == null) e.preventDefault()}}>
             <img className={styles.groupIcon} alt="" src="/group-80.svg" />
           </Link>
         </div>
