c69cd6d962b6260f8bc04b52f9961255242c3521
유우진 <ywj02120212@gmail.com>
2023-11-13 02:48:19 +0900

    add: Transiting

--- src/pages/Transiting.js
+++ src/pages/Transiting.js
@@ -1,15 +1,24 @@
 import styles from "./Transiting.module.css";
+import { Link } from "react-router-dom";
 
 const Transiting = () => {
   return (
-    <div className={styles.div}>
+    <>
       <div className={styles.groupParent}>
         <img className={styles.frameChild} alt="" src="/group-41.svg" />
         <div className={styles.lifelink}>LiFELiNK</div>
       </div>
-      <div className={styles.message}>이송중입니다!</div>
-      <button className={styles.button}>이송 중단하기</button>
-    </div>
+      <div className={styles.div}>
+
+        <p style={{ top: 80 }}>환자 등록이 완료되었습니다!</p>
+        <h2 style={{ top: 120 }}>결정한 병원으로<br/>환자를 이송 중입니다...</h2>
+
+        <Link to="/main">
+          <div className={styles.button}>이송을 중단하고 메인 메뉴로 돌아가기</div>
+        </Link>
+        
+      </div>
+    </>
   );
 };
 
